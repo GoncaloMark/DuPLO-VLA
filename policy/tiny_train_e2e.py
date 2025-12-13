fa99a793d4a9ba75dc8836aa00ddbdb24b3a89a8
GoncaloMark <gmgoncalo7@gmail.com>
2025-12-13 20:02:38 +0000

    maybe fixed

--- policy/tiny_train_e2e.py
+++ policy/tiny_train_e2e.py
@@ -50,7 +50,7 @@ def run_overfitting_test():
     
     # TINY DATASET
     max_train_episodes = 5  
-    batch_size = 32  
+    batch_size = 16
     num_workers = 2
     horizon = 16
     pad_before = 1
